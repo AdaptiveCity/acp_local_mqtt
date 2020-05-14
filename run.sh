977121dcba4a0bded39bd588f63dc426ba4bc192
acp_prod <acp_prod@tfc-app7.cl.cam.ac.uk>
2020-05-14 08:53:04 +0100

    run.sh logging change

--- run.sh
+++ run.sh
@@ -6,9 +6,24 @@
 ##################################
 
 # Find the directory this script is being run from
-# typically "/home/acp_prod/acp_local_mqtt/acp_decoders"
+# typically "/home/acp_prod/acp_local_mqtt"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$SCRIPT_DIR/acp_decoders/run.sh >>/var/log/acp_prod/acp_decoders.log 2>/var/log/acp_prod/acp_decoders.err
+# QUIT if acp_decoders is already running
+pid=$(pgrep -f "python3 acp_decoders.py")
+if [ $? -eq 0 ]
+then
+    echo $(date '+%s') $SCRIPT_DIR/run.sh FAIL: acp_decoders.py already running as PID $pid
+    echo $(date '+%s') $SCRIPT_DIR/run.sh FAIL: acp_decoders.py already running as PID $pid >>/var/log/acp_prod/acp_decoders.err
+    exit 1
+fi
+
+# otherwise, run acp_decoders
+
+cd $SCRIPT_DIR/acp_decoders
+
+source venv/bin/activate
+nohup python3 acp_decoders.py >>/dev/null 2>>/var/log/acp_prod/acp_decoders.err & disown
+exit 0
 
